313fa6d4a0e899736f2d339733452b6c93901525
Gino Messmer <git@ginomessmer.me>
2020-02-11 18:28:07 +0100

    Add project files.

---
+++ BlessedHomepage.API/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlessedHomepage.API.Controllers
+{
+    [Route("blog")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet("posts")]
+        public async Task<IActionResult> GetPosts()
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpPost("posts")]
+        public async Task<IActionResult> PostPost()
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpDelete("posts/{id}")]
+        public async Task<IActionResult> DeletePost()
+        {
+            return Ok();
+        }
+    }
+}
